4082b9455efe0d83af3f3922b8f7ae4172540b6a
Bikash Burma <bikashburma50@gmail.com>
2020-05-27 17:12:14 +0545

    fixes

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-import React from 'react';
+const ACTION_PERFORMED = {
+  STARTED: 'started',
+  STOPPED: 'stopped',
+  DELETED: 'deleted',
+  UPDATED: 'updated',
+  CREATED: 'added',
+};
 
-const Header = props => {  
-      <div className="header">
-        <div className="container header__container">
-          <div className="header__left">
-            <div className="logo">
-              <a href="/" title="Leapfrog Technology">
-                <img alt="Leapfrog Technology" />
-              </a>
-            </div>
-            <ul>
-              <li className="header__nav-item">
-                <a className="header__nav-item-link">
-                  Projects
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-item-link">
-                  Timesheet
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-item-link">
-                  Leave
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a
-                  className="header__nav-item-link header__nav-item-link--active"
-                >
-                  R&R
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-  };
-
-  export default Header;
+export  default ACTION_PERFORMED;
