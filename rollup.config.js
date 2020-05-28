d82a4b113511cfefa88bd8fbd33181c1a375f278
Nischal Shrestha <er.nischalshrestha@gmail.com>
2020-05-28 14:27:44 +0545

    test

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+const resolve = require("rollup-plugin-node-resolve");
+export default {
+  dest: "dist/main.js",
+  entry: "src/lib.js",
+  format: "cjs",
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel()
+  ]
+}
