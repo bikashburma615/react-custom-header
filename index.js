1cf96dd72e62c905e8e2a90327134ceb7165fe67
Nischal Shrestha <er.nischalshrestha@gmail.com>
2020-05-28 14:23:32 +0545

    test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import styles from './styles'
+import styles from "./styles";
+
+import Header from "./Header";
 
 const ACTION_PERFORMED = {
-  STARTED: 'started',
-  STOPPED: 'stopped',
-  DELETED: 'deleted',
-  UPDATED: 'updated',
-  CREATED: 'added',
+  STARTED: "started",
+  STOPPED: "stopped",
+  DELETED: "deleted",
+  UPDATED: "updated",
+  CREATED: "added",
 };
 
-export  default ACTION_PERFORMED;
-export const styles;
+export default ACTION_PERFORMED;
+export { Header };
